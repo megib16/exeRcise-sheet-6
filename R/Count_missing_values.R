6ee696c60fd96885f9d5ada09e670d0fd4716bc2
megib16 <megibida@Megis-MacBook-Air.local>
2025-07-20 14:56:06 +0200

    Another function

---
+++ R/Count_missing_values.R
@@ -0,0 +1,19 @@
+#' Count Missing (NA) Values in Each Column
+#'
+#' @param df A data frame.
+#' @return A data frame with the count of missing values per column.
+#' @examples
+#' count_missing(mtcars)
+#' df <- data.frame(a = c(1, NA, 3), b = c(NA, NA, 3))
+#' count_missing(df)
+#' @export
+count_missing <- function(df) {
+  missing_counts <- sapply(df,
+                           function(x) sum(is.na(x))
+                           )
+  data.frame(
+    Variable = names(missing_counts),
+    Missing = as.integer(missing_counts),
+    row.names = NULL
+  )
+}
