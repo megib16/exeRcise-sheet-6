016114fbf79e2f012f94c7d5ce7c739d9c671343
megib16 <megibida@Megis-MacBook-Air.local>
2025-07-20 13:58:49 +0200

    Some progress

--- R/Scatter_package.R
+++ R/Scatter_package.R
@@ -1,3 +1,7 @@
+#' Create a scatter plot with optional color and shape aesthetics
+#'
+#' This function creates a customizable scatter plot using ggplot2.
+#'
 #' @param data A data frame containing the data to plot.
 #' @param x_param The variable for the x-axis.
 #' @param y_param The variable for the y-axis.
@@ -14,6 +18,7 @@
 #' create_scatter_plot(mtcars, "mpg", "hp", color_var = "cyl", shape_var = "gear")
 #'
 #' @import ggplot2
+#' @export
 
 create_scatter_plot <- function(data,
                                 x_param,
@@ -23,3 +28,29 @@ create_scatter_plot <- function(data,
                                 title = "Scatter Plot",
                                 x_label = NULL,
                                 y_label = NULL)
+{
+  if (!is.null(shape_var) && !is.factor(data[[shape_var]])) {
+    data[[shape_var]] <- as.factor(data[[shape_var]])
+  }
+
+  p <- ggplot(data,
+              aes_string(x = x_param,
+                         y = y_param,
+                         color = color_var,
+                         shape = shape_var)
+  ) +
+    geom_point(size = 3) +
+    ggtitle(title) +
+    theme_minimal() +
+    theme(plot.title = element_text(hjust = 0.5))
+  if (!is.null(x_label)) {
+    p <- p + xlab(x_label)
+  }
+
+  if (!is.null(y_label)) {
+    p <- p + ylab(y_label)
+  }
+
+  return(p)
+}
+
