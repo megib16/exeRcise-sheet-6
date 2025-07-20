d16851774f97ec5f044919e36813052b9e3b319d
megib16 <megibida@Megis-MacBook-Air.local>
2025-07-20 14:26:35 +0200

    Format code style

--- R/Correlation_matrix.R
+++ R/Correlation_matrix.R
@@ -14,22 +14,38 @@
 #'
 #' @import ggplot2
 #' @importFrom reshape2 melt
-correlation_matrix_plot <- function(data, method = "pearson", title = "Correlation Matrix") {
+correlation_matrix_plot <- function(data,
+                                    method = "pearson",
+                                    title = "Correlation Matrix") {
 
-  num_data <- data[sapply(data, is.numeric)] # In order to select only numericc data
+  num_data <- data[sapply(data,
+                          is.numeric)] # In order to select only numericc columns
 
-  corr_mat <- cor(num_data, use = "pairwise.complete.obs", method = method)
+  corr_mat <- cor(num_data,
+                  use = "pairwise.complete.obs",
+                  method = method)
 
   corr_melt <- reshape2::melt(corr_mat)
 
-  ggplot(corr_melt, aes(Var1, Var2, fill = value)) +
+  ggplot(corr_melt,
+         aes(Var1,
+             Var2,
+             fill = value)
+         ) +
     geom_tile(color = "white") +
     scale_fill_gradient2(
-      low = "blue", high = "red", mid = "beige",
-      midpoint = 0, limit = c(-1,1), space = "Lab",
+      low = "blue",
+      high = "red",
+      mid = "beige",
+      midpoint = 0,
+      limit = c(-1,1),
+      space = "Lab",
       name = "Correlation"
     ) +
     theme_minimal() +
     ggtitle(title) +
-    theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust=1))
+    theme(axis.text.x = element_text(angle = 45,
+                                     vjust = 1,
+                                     hjust=1)
+          )
 }
